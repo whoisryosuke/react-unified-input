eb93ef0a623adc246f51aa6780be5a49ea0f18e9
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-10-17 16:20:38 -0700

    :construction: parent key

--- src/hooks/useFocusable.ts
+++ src/hooks/useFocusable.ts
@@ -1,21 +1,23 @@
 import { useEffect, useRef, useState } from "react";
 import { useLibraryStore } from "../store/library";
+import { useFocusContext } from "../context/FocusContext";
 
 const useFocusable = () => {
   const ref = useRef(null);
   const [focusId, setFocusId] = useState(Number(new Date()));
   const { focusItems, addFocusItem, removeFocusItem } = useLibraryStore();
+  const parentKey = useFocusContext();
 
   // Sync focus item with store
   useEffect(() => {
     if (!(focusId in focusItems)) {
-      addFocusItem(focusId, { parent: 0 });
+      addFocusItem(focusId, { parent: parentKey });
     }
 
     return () => {
       removeFocusItem(focusId);
     };
-  }, [focusId, addFocusItem, removeFocusItem, focusItems]);
+  }, [focusId, addFocusItem, removeFocusItem, focusItems, parentKey]);
 
   return {
     ref,
